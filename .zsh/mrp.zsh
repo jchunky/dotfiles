3ce3a4d45f2cd6fdba232bae0ae57cd12f7a27d0
Chunky <jasonc@nulogy.com>
2020-05-25 23:12:06 -0400

    Minor changes

--- .zsh/mrp.zsh
+++ .zsh/mrp.zsh
@@ -1,7 +1,10 @@
+# Front-end
 alias mrps='pushd client_nogulp/apps/mrp;npm start;popd'
 alias mrpbw='pushd client_nogulp/apps/mrp;npm run build:watch;popd'
 alias mrptw='pushd client_nogulp/apps/mrp;npm run test:watch;popd'
 alias mrpt='pushd client_nogulp/apps/mrp;npm run test;popd'
+
+# Back-end
 alias mrpu='pushd components/mrp;be rspec spec/unit;popd'
 alias mrpi='pushd components/mrp;be rspec spec/integration;popd'
 alias mrpa='mrpu;mrpi;'
