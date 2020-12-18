467448f62ce4d3225c6bf795f80b9488e7772241
Chunky <jasonc@nulogy.com>
2020-12-17 20:24:10 -0500

    Move ps_update into function to bypass jamf

---
+++ .zsh/production_scheduling.zsh
@@ -0,0 +1,10 @@
+function ps_update {
+  cd ~/src/production-scheduling/frontend
+  yarn install
+  cd ~/src/production-scheduling/backend
+  chruby `cat .ruby-version`
+  bundle
+  bundle exec rake db:schema:load
+  bundle exec rake dev:reset_seed
+  cd ..
+}
