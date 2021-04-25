fb2390d83bee1f2fc28aef8d06db6cae66de4759
Chunky <jasonc@nulogy.com>
2021-04-24 21:43:42 -0400

    Add symbolic link ~/.rubocop.yml

--- .zsh/symbolic_links.zsh
+++ .zsh/symbolic_links.zsh
@@ -1,5 +1,6 @@
 # Symbolic links
 # ln -s ~/Exercism ~/root/projects/exercism
+# ln -s ~/root/projects/dotfiles/.bin/.seed_ruby/.rubocop.yml ~/.rubocop.yml
 # ln -s ~/root/projects/private/.dotfiles/.nutrella.yml ~/.nutrella.yml
 # ln -s ~/root/projects/private/.dotfiles/.gitconfig.local ~/.gitconfig.local
 # ln -s ~/root/third_party/dev-toolbox/git-hooks/issue-number-prepend/commit-msg ~/.git-templates/hooks/commit-msg
