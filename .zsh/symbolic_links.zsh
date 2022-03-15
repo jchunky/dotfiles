7d2fae0f78b91fc8c3733558e0a29c6d64efbe64
Chunky <jasonc@nulogy.com>
2022-03-15 00:42:44 -0400

    Document subl symbolic link; rename oc_* aliases to pm_*

--- .zsh/symbolic_links.zsh
+++ .zsh/symbolic_links.zsh
@@ -1,6 +1,7 @@
 # Symbolic links
+# ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
 # ln -s ~/Exercism ~/root/projects/exercism
 # ln -s ~/root/projects/dotfiles/.bin/.seed_ruby/.rubocop.yml ~/.rubocop.yml
-# ln -s ~/root/projects/private/.dotfiles/.nutrella.yml ~/.nutrella.yml
 # ln -s ~/root/projects/private/.dotfiles/.gitconfig.local ~/.gitconfig.local
+# ln -s ~/root/projects/private/.dotfiles/.nutrella.yml ~/.nutrella.yml
 # ln -s ~/root/third_party/dev-toolbox/git-hooks/issue-number-prepend/commit-msg ~/.git-templates/hooks/commit-msg
