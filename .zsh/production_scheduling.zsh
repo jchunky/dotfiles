9fb1f9333de9d6c9a1c863c696b8569eeac9a6d9
Chunky <jasonc@nulogy.com>
2022-04-06 05:34:59 -0400

    Rename psu to ps_update. Change ps_update to call 'rake db:reset'.

--- .zsh/production_scheduling.zsh
+++ .zsh/production_scheduling.zsh
@@ -4,15 +4,14 @@ export MESSAGE_BUS_CONSUMER_GROUP_ID=chunky
 alias ps_cop='rubocop -A'
 alias ps_test='rake test:rspec'
 
-function psu {
+function ps_update {
   cd ~/src/production-scheduling/frontend
   yarn install
   cd ~/src/production-scheduling/backend
   chruby `cat .ruby-version`
   bundle
   bundle exec spring stop
-  bundle exec rake db:test:prepare
-  bundle exec rake db:schema:load
+  bundle exec rake db:reset
   bundle exec rake dev:reset_seed
   cd ..
 }
