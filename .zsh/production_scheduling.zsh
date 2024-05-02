01cf46e452e97f92b0eab7df4da4e72b2f07445c
Chunky <jasonc@nulogy.com>
2024-05-02 13:12:58 -0400

    Update 'ps_e2e' alias to work without docker

--- .zsh/production_scheduling.zsh
+++ .zsh/production_scheduling.zsh
@@ -3,7 +3,7 @@ export ESLINT_NO_DEV_ERRORS=true
 export MESSAGE_BUS_CONSUMER_GROUP_ID=chunky
 
 alias ps_cop='bundle exec rubocop -A'
-alias ps_e2e='scripts/run_e2e_server.sh'
+alias ps_e2e='PORT=3031 REACT_APP_E2E_TEST_MODE="local" yarn run install-and-start'
 alias ps_test='rake test:rspec'
 
 function ps_reset {
