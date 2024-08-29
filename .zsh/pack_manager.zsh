b3aa899524d068c8f60a675dd3c17772614bd8ca
Jason CheongKeeYou <jasonc@nulogy.com>
2024-08-29 13:18:44 -0400

    Update pm_update to do db:seed:replant

--- .zsh/pack_manager.zsh
+++ .zsh/pack_manager.zsh
@@ -59,7 +59,7 @@ alias pm_seed_candy_pack='rake nulogy:tdc:candy_pack:schedule:fs_variety_mix'
 alias pm_seed_choco_pack='rake nulogy:tdc:choco_pack:schedule:fs_variety_mix'
 alias pm_seed_dane_post='rake nulogy:tdc:dane_post:schedule:fs_variety_mix'
 alias pm_seed_jit='pm_seed_choco_pack'
-alias pm_update='bundle; pmu; rake db:test:prepare'
+alias pm_update='bundle; pmu; RAILS_ENV=test rails db:seed:replant'
 alias rn='RAILS_NEXT=true '
 alias seed_work_orders='rails runner development/scripts/seed_scripts/seed_work_orders.rb'
 
